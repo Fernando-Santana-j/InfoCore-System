c82cfe7ae0500422fca397ef5ee0f4bb7dabf1ca
Fernando <fernandoj132sj@gmail.com>
2026-04-05 23:12:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+//TODO-------------importes------------
+const express = require('express')
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const path = require('path');
+const multer = require('multer')
+const cookieParser = require("cookie-parser");
+// const config = require('./config/config.json');
+
+//TODO------------Configs--------------
+
+const app = express();
+
+app.use(session({
+    secret: process.env.SECRET || 'infocore-fajg3bi2bt3fi3nt2fajbf2',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 3600000
+    }
+}));
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.use(express.static('src'));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'src')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/uploads/')
+    },
+    filename: function (req, file, cb) {
+        const codigo = require('crypto').randomBytes(42).toString('hex');
+        const originalName = file.originalname;
+        const extension = originalName.substr(originalName.lastIndexOf('.'));
+        const fileName = codigo + extension;
+        cb(null, `${fileName}`)
+    }
+});
+
+const upload = multer({ storage });
+
+
+
+
+//TODO------------WEB PAGE--------------
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(3131, () => {
+    const dataHora = new Date();
+    const formatado = d => ('0' + d).slice(-2);
+    const dataHoraFormatada = `${formatado(dataHora.getDate())}/${formatado(dataHora.getMonth() + 1)}/${dataHora.getFullYear()} ${formatado(dataHora.getHours())}:${formatado(dataHora.getMinutes())}:${formatado(dataHora.getSeconds())}`;
+    console.log(`${dataHoraFormatada} [WEB-SYSTEM] Servidor rodando na porta 3131`);
+});
+
